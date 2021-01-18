6438c15d76b93758466dd43c76bb3694a1311990
Regis Araujo Melo <regis@r3tecnologia.net>
2021-01-18 18:08:47 -0300

    structure base project

---
+++ hooks/post_gen_project.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+GREEN='\033[0;32m'
+echo -e "${GREEN}Project successfuly generated!"
+
+# bundle install
+bundle exec pod install
+
+echo -e "${GREEN}Pods successfuly installed!"
---
+++ {{cookiecutter.app_name}}/{{cookiecutter.app_name}}/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  {{cookiecutter.app_name}}
+//
+//  Created by {{cookiecutter.lead_dev}} on {% now 'local', '%d.%m.%Y' %}.
+//  Copyright © {% now 'local', '%Y' %} {{cookiecutter.company_name}}. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
